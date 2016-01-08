e794246cb5b6bcaae87520b028a9111b2f96c2e5
francisco2016 <otrayama@gmail.com>
2016-01-08 13:36:04 +0100

    Creada cl Usuario con atr. y constructor

--- Alimento.java
+++ Alimento.java
@@ -2,7 +2,7 @@
 /**
 aplicación destinada a personas que están a dieta y que necesitan contar las calorías y los macronutrientes de 
 los alimentos que ingieren
- * @author (VACACIONES,   VACACIONES.) 
+ * @author (pavilion,  pavilion) 
  * @version (07   /01   /2016)
  */
 public class Alimento
@@ -15,37 +15,70 @@ public class Alimento
     public float carbohidratos;
     // At para guardar el valor de las grasas.
     public float grasas;
-
+    // At para guardar el valor de las calorias.
+    public float calorias;
     /**
      * Constructor for objects of class Alimento
      */
-    public Alimento(String nombreAl, float proteinas, float carbohidratos, float grasas)
+    public Alimento(String nombreAl, float proteinas, float carbohidratos, float grasas )
     {
         this.nombreAl =  nombreAl;
         this.proteinas = proteinas;
         this.carbohidratos = carbohidratos;
         this.grasas = grasas;
+        calorias = ( ((proteinas + carbohidratos) *4) + grasas * 9);
     }
      
     /**
      * Mt para mostrar información nutricional de los alimentos.
      */
     public void muestraDatos(){
-        System.out.println("Nombre:                         " +nombreAl);
+        //System.out.println("Nombre:                         " +nombreAl);
         System.out.println("");
         System.out.println("Proteinas por cada 100 gr:      " +proteinas    );
         System.out.println("Carbohidratos por cada 100 gr:  " +carbohidratos);
         System.out.println("Grasas por cada 100 gr:         " +grasas);
         System.out.println("Total de calorias:              " +sumaCalorias());
-        System.out.println("Macronutriente/s mayoritario/s: " +nutrienteMayor());
+        //System.out.println("Macronutriente/s mayoritario/s: " +nutrienteMayor());
     }
     
+    /**
+     * Serie de mt. para asignar valor a los atributos.
+     */
+    public void setProteinas(float proteinasP){
+        proteinas = proteinasP;
+    }
+     public void setCarbohidratos(float carbohidratosC){
+        carbohidratos = carbohidratosC;
+    }
+     public void setGrasas(float grasasG){
+        grasas = grasasG;
+    }
+    
+    
+    /**
+     * serie de mt. para retornar el valor de los atributos
+     */
+    public float getProteinas(){
+        return proteinas;
+    }
+    public float getCarbohidratos(){
+        return carbohidratos;
+    }
+    public float getGrasas(){
+        return grasas;
+    }
+    public float getCalorias(){
+        return calorias;
+    }
+    
+     
         /**
      * mt para sumar las calorias totales de cada alimento.
      */
     public float sumaCalorias(){
-        float totalCalorias = ( ((proteinas + carbohidratos) *4) + grasas * 9);
-        return totalCalorias;
+        float calorias = ( ((proteinas + carbohidratos) *4) + grasas * 9);
+        return calorias;
     }
     
     /**
@@ -73,7 +106,8 @@ public class Alimento
         }
         return nutriente;
     }
-
+    
+    
     
    }
 
---
+++ Usuario.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class Usuario here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Usuario
+{
+    //At para guardar el nombre del usuario.
+    private String nombreUs;
+    // At para guardar el valor de las proteinas.
+    private float proteinas;
+    // At para guardar el valor de los carbohidratos.
+    private float carbohidratos;
+    // At para guardar el valor de las grasas.
+    private float grasas;
+    // At para guardar el valor de las calorias.
+    private float calorias;
+
+    /**
+     * Constructor for objects of class Usuario
+     */
+    public Usuario(String nombreUs)
+    {
+        this.nombreUs = nombreUs;
+       proteinas = 0;
+       carbohidratos = 0;
+       grasas = 0;
+       calorias = 0;
+    }
+
+   
+}
+
+
+
+
+
+
+
+
